abb2b46f4e85d793fea3614b3213e695e1b3e3d3
Gonzalo <gonzaloburgosdelahera@|gmail.com>
2022-08-23 17:43:47 +0200

    A

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 mod hardware;
 mod util;
 
-// PRUEBA
+use std::sync::{Arc, Mutex};
 
+use eframe::{run_native, NativeOptions, App};
 use hardware::sys::System;
 
+struct GUI;
+
+impl App for GUI {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        
+    }
+}
+
 fn main() {
+    let native_options = NativeOptions::default();
+    let app = GUI;
+
+    // let mut sys = Arc::new(Mutex::new(System::new()));
+
     let mut sys = System::new();
-    
-    sys.run();
+
+    run_native("IBM PC", native_options, Box::new(|_cc| Box::new(GUI)));
+    // sys.run();
 }

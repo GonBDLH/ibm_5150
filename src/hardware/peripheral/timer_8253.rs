effb243fb05ff9c8cb2927115cbbb5737ae0737d
Gonzalo <gbd1004@alu.ubu.es>
2022-11-14 19:30:50 +0100

    Revertidos cambios en el timer

--- src/hardware/peripheral/timer_8253.rs
+++ src/hardware/peripheral/timer_8253.rs
@@ -72,7 +72,9 @@ impl TIM8253 {
                     // let _b = 0;
 
                     if dif.1 {
-                        self.channels[channel].out = true;
+                        if channel == 0 {
+                            pic.irq(IRQs::Irq0);
+                        }
                     }
                 },
                 Mode::Mode1 => {},
@@ -83,15 +85,15 @@ impl TIM8253 {
             }
         }
 
-        if self.channels[0].out {
-            pic.irq(IRQs::Irq0);
-        }
-        if self.channels[1].out {
+        // if self.channels[0].out {
+        //     pic.irq(IRQs::Irq0);
+        // }
+        // if self.channels[1].out {
 
-        }
-        if self.channels[2].out {
+        // }
+        // if self.channels[2].out {
 
-        }
+        // }
     }
 }
 

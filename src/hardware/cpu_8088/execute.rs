1b99e646975df8fb6bf010ad5e0c6cf6839696d9
Gonzalo <gonzaloburgosdelahera@|gmail.com>
2023-04-02 20:26:00 +0200

    change add/adc

--- src/hardware/cpu_8088/execute.rs
+++ src/hardware/cpu_8088/execute.rs
@@ -115,21 +115,46 @@ impl CPU {
             Opcode::ADD => {
                 let val1 = self.get_val(bus, self.instr.operand1);
                 let val2 = self.get_val(bus, self.instr.operand2);
-                let res = val1.overflowing_add(val2);
+                let res = add(val1, val2, self.instr.data_length);
                 self.set_val(bus, self.instr.operand1, res.0);
                 self.flags
                     .set_add_flags(self.instr.data_length, val1, val2, res.0, res.1)
             }
             Opcode::ADC => {
+                // let val1 = self.get_val(bus, self.instr.operand1);
+                // let val2 = self
+                //     .get_val(bus, self.instr.operand2)
+                //     .overflowing_add(self.flags.c as u16);
+                // let res = val1.overflowing_add(val2.0);
+                // self.set_val(bus, self.instr.operand1, res.0);
+                // self.flags
+                //     .set_add_flags(self.instr.data_length, val1, val2.0, res.0, res.1 | val2.1)
                 let val1 = self.get_val(bus, self.instr.operand1);
-                let val2 = self
-                    .get_val(bus, self.instr.operand2)
-                    .overflowing_add(self.flags.c as u16);
-                let res = val1.overflowing_add(val2.0);
+                let val2 = self.get_val(bus, self.instr.operand2);
+                let cflag = self.flags.c as u16;
+                let res = adc(val1, val2, cflag, self.instr.data_length);
                 self.set_val(bus, self.instr.operand1, res.0);
                 self.flags
-                    .set_add_flags(self.instr.data_length, val1, val2.0, res.0, res.1 | val2.1)
+                    .set_add_flags(self.instr.data_length, val1, val2.wrapping_add(cflag), res.0, res.1)
             }
+            // Opcode::ADD => {
+            //     let val1 = self.get_val(bus, self.instr.operand1);
+            //     let val2 = self.get_val(bus, self.instr.operand2);
+            //     let res = val1.overflowing_add(val2);
+            //     self.set_val(bus, self.instr.operand1, res.0);
+            //     self.flags
+            //         .set_add_flags(self.instr.data_length, val1, val2, res.0, res.1)
+            // }
+            // Opcode::ADC => {
+            //     let val1 = self.get_val(bus, self.instr.operand1);
+            //     let val2 = self
+            //         .get_val(bus, self.instr.operand2)
+            //         .overflowing_add(self.flags.c as u16);
+            //     let res = val1.overflowing_add(val2.0);
+            //     self.set_val(bus, self.instr.operand1, res.0);
+            //     self.flags
+            //         .set_add_flags(self.instr.data_length, val1, val2.0, res.0, res.1 | val2.1)
+            // }
             Opcode::INC => {
                 let val = self.get_val(bus, self.instr.operand1);
                 let res = val.wrapping_add(1);

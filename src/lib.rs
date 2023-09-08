3412f8496ae07d652703bc954a36f557778c170c
Gonzalo <gonzaloburgosdelahera@|gmail.com>
2023-09-08 04:06:25 +0200

    Prueba tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,26 @@ pub mod util;
 
 pub mod debugger;
 pub mod screen;
+
+#[cfg(test)]
+mod tests {
+    use std::{fs::File, io::Read};
+    use serde_json::Value;
+
+
+    fn read_file(filename: &str) -> Value {
+        let mut file = File::open(filename).expect("JSON no existe");
+        let mut file_string = String::new();
+
+        file.read_to_string(&mut file_string).expect("Error leyendo el JSON");
+
+        serde_json::from_str(&file_string).unwrap()
+    }
+
+    #[test]
+    fn test_00() {
+        let file = read_file("prueba.json");
+
+        println!("{:#?}", file)
+    }
+}
